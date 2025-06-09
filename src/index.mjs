4fb1c1d75c7d8b4862e60276da6eead5981b1f22
ThanonC <126615459+ThanonC@users.noreply.github.com>
2025-06-09 22:29:06 +0200

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,19 @@
 import e from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cassandra from "cassandra-driver";
+
 
 const port = 7600;
 const app = e();
 const server = http.createServer(app);
 const io = new Server(server);
+
+const cluster = new cassandra.Client({
+    contactPoints: ['localhost'],
+    localDataCenter: 'datacenter1',
+    keyspace: 'users'
+});
 
 app.get('/status', (req, res) => {
     res.json({ 'Status': 'Running'});
@@ -18,12 +26,14 @@ const playerLoc = {
     z: 0,
 }
 
-io.on('connect', (socket) => {
+io.on('connection', (socket) => {
     console.log(`Player with session id ${socket.id} joined`);
-    io.emit(`playerJoined', 'player with ${socket.id} joined`);
+
+    
+
     socket.on('playerLocation', (x, y, z) => {
         playerLoc[socket.id, x, y, z];
-    })
+    });
 
     socket.on("disconnect", () => {
         console.log(`Player with session id ${socket.id} disconnected`);
@@ -34,3 +44,8 @@ io.on('connect', (socket) => {
 server.listen(port, () => {
     console.log(`Running on Port ${port}`);
 });
+
+
+function createAccount(uuid, username, firstname, lastname, email) {
+    cluster.execute('INSERT INTO players (user_id, username, firstname, lastname, email, money, mana, health, shield, locx, locy, locz, characterclass, charactername) (')
+}
